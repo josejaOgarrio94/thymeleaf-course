7d8be891ccbeb50339ca369d6f8defe624a2361b
josejaOgarrio94 <javiergomezlastra@hotmail.com>
2024-09-11 20:49:33 +0200

    Primera subida del código base

---
+++ src/test/java/joseja/springframework/thymeleaf_course/ThymeleafCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package joseja.springframework.thymeleaf_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
